2161d5ce8cb4324f25632328c2a941a0b6b22372
Chitransh Jain <chitransh0206jain@gmail.com>
2026-07-24 12:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (26.61%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -10,45 +10,66 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
+    // ListNode* reverseList(ListNode* head) {
+    //     ListNode* prev = nullptr;
+    //     ListNode* curr = head;
 
-        while (curr != nullptr) {
-            ListNode* next = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = next;
-        }
+    //     while (curr != nullptr) {
+    //         ListNode* next = curr->next;
+    //         curr->next = prev;
+    //         prev = curr;
+    //         curr = next;
+    //     }
 
-        return prev;
-    }
+    //     return prev;
+    // }
 
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if (head == nullptr) {
-            return head;
+    //     if (head == nullptr) {
+    //         return head;
+    //     }
+
+    //     // 1. Reverse the list
+    //     head = reverseList(head);
+
+    //     // 2. Delete nth node from the beginning
+    //     if (n == 1) {
+    //         head = head->next;
+    //     }
+    //     else {
+    //         ListNode* curr = head;
+
+    //         for (int i = 1; i < n - 1; i++) {
+    //             curr = curr->next;
+    //         }
+
+    //         curr->next = curr->next->next;
+    //     }
+
+    //     // 3. Reverse again
+    //     head = reverseList(head);
+
+    //     return head;
+
+
+        if(head==nullptr || head->next==nullptr) return nullptr;
+        ListNode* fast=head;
+        ListNode* curr=head;
+        ListNode* slow=curr;
+        // slow->next=head;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
         }
-
-        // 1. Reverse the list
-        head = reverseList(head);
-
-        // 2. Delete nth node from the beginning
-        if (n == 1) {
-            head = head->next;
+        while(fast!=nullptr){
+            slow=curr;
+            curr=curr->next;
+            // slow=slow->next;
+            fast=fast->next;
         }
-        else {
-            ListNode* curr = head;
-
-            for (int i = 1; i < n - 1; i++) {
-                curr = curr->next;
-            }
-
-            curr->next = curr->next->next;
+        if(head==curr){
+            head=head->next;
         }
-
-        // 3. Reverse again
-        head = reverseList(head);
-
+        slow->next=curr->next;
         return head;
     }
 };
